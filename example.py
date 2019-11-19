0a236d8072e5cda3ec6748aa421debdbbb6b269d
Luke <lsh-0@users.noreply.github.com>
2019-11-19 19:13:04 +1030

    added new (breaking) case to example.py -network connection isn't shared

--- example.py
+++ example.py
@@ -1,16 +1,8 @@
 from threadbare.state import settings
 from threadbare.operations import remote
 
-def main():
-    # it's embarassing how nice it is to play with global state...
-    with settings(user='elife', host='34.201.187.7', quiet=False, discard_output=False) as env:
-        #result = remote(r'echo -e "\e[31mRed Text\e[0m"', use_shell=False)
-        #result = remote('echo "standard out"; echo "sleeping"; sleep 2; >&2 echo "standard error"; exit 2', combine_stderr=False)
-        #result = remote_sudo('salt-call state.highstate')
-        result = remote('foo=bar; echo "bar? $foo!"', use_shell=False)
-        # read from stdin
-        #result = remote('echo "> "; cat -')
-
+def handle_result(result):
+    with settings() as env:
         if env.get('quiet', False) and not env.get('discard_output', False):
             print('---')
             for line in result['stdout']:
@@ -22,6 +14,28 @@ def main():
         print('---')
         
         print('results:',result)
-    
+
+def main():
+    # it's embarassing how nice it is to play with global state...
+    with settings(user='elife', host='34.201.187.7', quiet=False, discard_output=False) as env:
+        #result = remote(r'echo -e "\e[31mRed Text\e[0m"', use_shell=False)
+        #result = remote('echo "standard out"; echo "sleeping"; sleep 2; >&2 echo "standard error"; exit 2', combine_stderr=False)
+        #result = remote_sudo('salt-call state.highstate')
+        result = remote('foo=bar; echo "bar? $foo!"', use_shell=False)
+        # read from stdin
+        #result = remote('echo "> "; cat -')
+
+        # these should all re-use the same network connection
+        # (they don't, see TODO)
+        command_list = [
+            "echo hello,",
+            "echo world.",
+            "echo how",
+            "echo are",
+            "echo you?",
+        ]
+        for command in command_list:
+            handle_result(remote(command))
+
 if __name__ == '__main__':
     main()
