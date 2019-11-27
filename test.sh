5257c00b326110603804a888d5b8306956b92b09
Luke <lsh-0@users.noreply.github.com>
2019-11-27 17:45:18 +1030

    test script now defaults to using tox in parallel unless 'dev' is used

--- test.sh
+++ test.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 set -e
-source venv/bin/activate
-PYTHONPATH=threadbare/ python -m pytest tests/ -vv "$@"
+
+# remove any old compiled python files
+# pylint likes to lint them, pytest likes to test them
+find threadbare/ -name '*.py[c|~]' -delete
+find tests/ -name '*.py[c|~]' -delete
+find threadbare/ -regex "\(.*__pycache__.*\|*.py[co]\)" -delete
+find tests/ -regex "\(.*__pycache__.*\|*.py[co]\)" -delete
+
+dev=${1:-""}
+if [ "$dev" = "" ]; then
+    tox --parallel auto
+else
+    shift # pop the first arg off
+    source venv/bin/activate
+    PYTHONPATH=threadbare/ python -m pytest tests/ -vv "$@"
+fi
