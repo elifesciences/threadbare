42fac6e8eec8718f4cea4add266474b9afa3eb7f
Luke <lsh-0@users.noreply.github.com>
2020-01-30 16:45:41 +1030

    type checking now accounts for python2 strings

--- tests/test_common.py
+++ tests/test_common.py
@@ -61,7 +61,8 @@ def test__shell_escape():
 
 
 def test__shell_escape_bad_cases():
-    bad_types = [[None, TypeError], [b"", TypeError]]
+    # [b"", TypeError]] # b-strings are strings in py2
+    bad_types = [[None, TypeError]]
     for given, expected in bad_types:
         with pytest.raises(expected):
             common._shell_escape(given)
