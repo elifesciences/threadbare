8d54a8f3b36fa8f0131e42192c5f16e2104e0224
Luke <lsh-0@users.noreply.github.com>
2020-01-31 15:25:46 +1030

    bumps version in setup.py to 1.0.0

--- setup.py
+++ setup.py
@@ -2,6 +2,18 @@ from setuptools import setup
 
 setup(
     name='threadbare',
-    version='0.1.0',
-    install_requires=["parallel-ssh==1.9.1"]
+    version='1.0.0',
+    description='A Fabric and Paramiko replacement library',
+    long_description="A partial replacement of the Fabric/Paramiko library utilising ParallelSSH.",
+    url="https://github.com/elifesciences/threadbare",
+    maintainer="Luke (lsh)",
+    install_requires=["parallel-ssh==1.9.1"],
+    classifiers=[
+        "Intended Audience :: System Administrators",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    license="GPLv3"
 )
