d7997752fab494a9648bd014c5b0995728940d30
Luke <lsh-0@users.noreply.github.com>
2020-09-11 16:38:54 +0930

    fixes python2 requirements.

--- install.sh
+++ install.sh
@@ -5,10 +5,9 @@ env=${1:-} # "-dev" or nothing
 . mkvenv.sh
 
 source venv/bin/activate
-pip install -r "requirements$env.txt"
 
 if [ -e venv/bin/python2.7 ]; then
-    # requirements-py2.txt
-    # requirements-dev-py2.txt
-    pip install -r "requirements$env-py2.txt"
+    pip install -r "requirements-py2.txt"
+else
+    pip install -r "requirements$env.txt"
 fi
