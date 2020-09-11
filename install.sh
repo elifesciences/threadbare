bf1fe0743ed75580b99c8768ebd875e377a7ca1d
Luke <lsh-0@users.noreply.github.com>
2020-09-11 16:04:41 +0930

    adds a Pipfile and updates requirements files

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+env=${1:-} # "-dev" or nothing
+
 . mkvenv.sh
 
 source venv/bin/activate
-pip install -r requirements.txt
+pip install -r "requirements$env.txt"
 
 if [ -e venv/bin/python2.7 ]; then
-    pip install -r requirements-py2.txt
+    # requirements-py2.txt
+    # requirements-dev-py2.txt
+    pip install -r "requirements$env-py2.txt"
 fi
