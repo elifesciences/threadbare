06fc2f873ce36a66f08cd752566a85e6d1818001
Luke <lsh-0@users.noreply.github.com>
2020-09-11 16:31:41 +0930

    fixes a python2 conditional that slipped in

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -1,7 +1,10 @@
 import pytest
 import time
 import logging
-from unittest.mock import patch
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
 from threadbare import execute, operations
 from threadbare.state import settings
 from threadbare.common import PromptedException
