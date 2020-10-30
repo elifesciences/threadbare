b7023a762a0d1e117cd13b965a417d9952ace0fa
Luke <lsh-0@users.noreply.github.com>
2020-10-30 15:48:09 +1030

    adds update-dependencies-py2.sh script and bumps cryptography for py2

---
+++ update-dependencies-py2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# quick and dirty script for updating requirements-py2.txt
+
+rm -rf venv/
+virtualenv2 venv
+source venv/bin/activate
+pip install wheel pip --upgrade
+VIRTUAL_ENV="venv" pipenv sync
+pip install pytest pytest-cov mock
