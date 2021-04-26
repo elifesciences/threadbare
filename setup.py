3d8d7e3dc4ded968919e062f2690580c6be5b4e5
Luke <lsh-0@users.noreply.github.com>
2021-04-26 14:52:24 +0930

    bumps release to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='threadbare',
-    version='1.1.4',
+    version='2.0.0',
     description='A Fabric and Paramiko replacement library',
     long_description="A partial replacement of the Fabric/Paramiko library utilising ParallelSSH.",
     long_description_content_type="text/markdown",
