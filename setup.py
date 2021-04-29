5f3d0cc57ad212830b393b7e2e9b47f59efabfb4
Luke <lsh-0@users.noreply.github.com>
2021-04-29 13:13:33 +0930

    bumps parallel-ssh to 1.10.* for python3.8 support. relaxes restriction on version of parallel-ssh installed in setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='threadbare',
-    version='2.0.0',
+    version='2.0.1',
     description='A Fabric and Paramiko replacement library',
     long_description="A partial replacement of the Fabric/Paramiko library utilising ParallelSSH.",
     long_description_content_type="text/markdown",
     url="https://github.com/elifesciences/threadbare",
     maintainer="Luke",
     maintainer_email="lsh-0@users.noreply.github.com",
-    install_requires=["parallel-ssh==1.9.1"],
+    install_requires=["parallel-ssh>=1.9.1"],
     packages=["threadbare"],
     classifiers=[
         "Intended Audience :: System Administrators",
