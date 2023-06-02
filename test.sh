fb10f091de10b727cd70d6629ce6c407d643ab7d
Luke <lsh-0@users.noreply.github.com>
2023-06-02 12:23:21 +0930

    testing, removes mentions of 'tox'

--- test.sh
+++ test.sh
@@ -8,25 +8,19 @@ find tests/ -name '*.py[c|~]' -delete
 find threadbare/ -regex "\(.*__pycache__.*\|*.py[co]\)" -delete
 find tests/ -regex "\(.*__pycache__.*\|*.py[co]\)" -delete
 
-dev=${1:-""}
-if [ "$dev" = "dev" ]; then
-    shift # pop the first arg off
-    source venv/bin/activate
-    if [ -z "$@" ]; then
-        # no further args passed to test script
-        # run with coverage and reporting enabled
-        PYTHONPATH=threadbare/ python -m pytest \
-            tests/ \
-            -vv \
-            --cov=threadbare/ \
-            --cov-report html --cov-report term \
-            --cov-fail-under 75
-    else
-        # running tests adhoc, skip coverage reporting
-        PYTHONPATH=threadbare/ python -m pytest \
-            -vv \
-            "$@"
-    fi
+source venv/bin/activate
+if [ -z "$@" ]; then
+    # no further args passed to test script
+    # run with coverage and reporting enabled
+    PYTHONPATH=threadbare/ python -m pytest \
+        tests/ \
+        -vv \
+        --cov=threadbare/ \
+        --cov-report html --cov-report term \
+        --cov-fail-under 75
 else
-    tox --parallel auto "$@"
+    # running tests adhoc, skip coverage reporting
+    PYTHONPATH=threadbare/ python -m pytest \
+        -vv \
+        "$@"
 fi
